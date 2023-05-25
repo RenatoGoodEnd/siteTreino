3e21c24b2770db2ca78284b0f3e91c0ce289cfd3
RenatoGoodEnd <m.renatoihr@hotmail.com>
2023-05-25 20:11:00 -0300

    ajuste de array

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-const exercicio = document.querySelector('#teste')
+const exercicio = document.querySelectorAll('.tabela__exercicio')
 const listaDeExercicio = JSON.parse(localStorage.getItem("lista")) || []
-exercicio.value = listaDeExercicio.exercicio
-	
-exercicio.addEventListener('focusout', () => {
-    const elemento = exercicio.value;
-    
-	const novoItem = {
-        "exercicio": elemento,
-        //"peso": peso
-    }
-	
-    localStorage.setItem("lista", JSON.stringify(novoItem))
-    
-    });
+console.log(exercicio[2])
+exercicio.forEach(teste)
+//exercicio.value = listaDeExercicio.exercicio
 
+function teste(valor, indice, arr){
+    exercicio[indice].value = listaDeExercicio.treinoA.exercicio
+    exercicio[indice].addEventListener('focusout', () => {
+        console.log(exercicio[indice].value)
+  
+
+        const elemento = exercicio[indice].value;
+        
+        const novoItem = {
+            "treinoA": [
+                {
+                    "id": indice,
+                    "exercicio": elemento,
+                    "peso": "" 
+                }
+            ]
+            //"exercicio": elemento,
+            //"peso": peso
+        }
+        
+        localStorage.setItem("lista", JSON.stringify(novoItem))
+        
+        });
+}
 
