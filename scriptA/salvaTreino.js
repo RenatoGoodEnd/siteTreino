4607813877ceb13846ae393c5e7a68e99f9e0a90
RenatoB_Alves <44118628+RenatoGoodEnd@users.noreply.github.com>
2023-06-13 16:26:38 -0300

    alteração teste scriptA

--- scriptA/salvaTreino.js
+++ scriptA/salvaTreino.js
@@ -2,16 +2,33 @@ function salvarTreino(valor, indice, arr){
     arr[indice].addEventListener('focusout', () => {
         console.log(lista.value)
 		const elementoTreino = exercicio[indice].value;
-        const elementoPeso = listaDeExercicio[0].peso;
-        const elementoLista = listaDeExercicio[0].lista;
-        const novoItem = 
-                {
-                    "lista": elementoLista,
-                    "id": indice,
-                    "treino": elementoTreino,
-                    "peso": elementoPeso
-                }
-        listaDeExercicio.splice(indice, 1, novoItem);
+        const novoId = valor.id
+        const existe = listaDeExercicio[indice].find(elemento => elemento.id === novoId)
+
+        if(existe){
+            listaDeExercicio[indice].treino = elementoTreino
+        }else{
+            // const elementoPeso = listaDeExercicio[0].peso;
+            // const elementoLista = listaDeExercicio[0].lista;
+            const novoItem = 
+                    {
+                        "lista": "A",
+                        "id": indice,
+                        "treino": elementoTreino,
+                        "peso": ""
+                    }
+            listaDeExercicio.splice(indice, 1, novoItem);
+        }
         localStorage.setItem("exercicios", JSON.stringify(listaDeExercicio));
     });
 }
+
+
+
+
+
+// se existe elemento na lista listaDeExercicio.id === valor.id
+
+// se sim atualiza o item com o que consta no valor recebido
+
+// se não cria um novo item completo
