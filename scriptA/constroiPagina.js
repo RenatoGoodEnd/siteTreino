37bf1cafa336a3d8e9f0c20643bd4b4945a63caf
RenatoB_Alves <44118628+RenatoGoodEnd@users.noreply.github.com>
2023-06-22 17:09:50 -0300

    corrigido textarea

--- scriptA/constroiPagina.js
+++ scriptA/constroiPagina.js
@@ -3,9 +3,7 @@ function construirPagina(listaDeTreino){
         elementoDoTreino.innerHTML += `
         <tr>
         <td class="tabela__elemento">
-            <textarea name="Exercício" onkeypress="digitar(event)" tabIndex="${element.id}" class="tabela__exercicio" cols="17" rows="2">
-            ${element.treino}
-            </textarea>
+            <textarea name="Exercício" onkeypress="digitar(event)" tabIndex="${element.id}" class="tabela__exercicio" cols="17" rows="2">${element.treino}</textarea>
         </td>
         <td class="tabela__elemento">
             <input type="number" name="Peso" class="tabela__peso" cols="2" rows="1" value="${element.peso}">
